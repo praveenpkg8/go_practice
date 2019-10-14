b510b4c4ea0c04f0a940f2f3cbfe18d4c6c29d4b
Your-Full-Name <praveenpkg8@gmail.com>
2019-10-14 23:32:51 +0530

    working on basic of data formator

---
+++ basic_types_03/numeric_types/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	var (
+		randomInt8 int8 = 97
+		randomInt  int  = 45
+		randomUint uint = 555
+	)
+	fmt.Printf("%d, %d, %d\n", randomInt, randomInt8, randomUint)
+}
