fb21e14e42d4e25bc8c012144d1e364b5522b926
Your-Full-Name <praveenpkg8@gmail.com>
2019-10-14 23:40:51 +0530

    working on operation in go

---
+++ basic_types_03/operations/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	var a, b int = 2, 3
+	var sol1 int = (a + b) * (a - b)
+
+	a++
+	b += 10
+
+	var sol2 int = a ^ b
+
+	var r = 3.5
+
+	var sol3 = math.Pi * r * r
+
+	fmt.Printf(" solution 1 : %d \n solution 2 : %d \n solution 3 : %f \n", sol1, sol2, sol3)
+}
