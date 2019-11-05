549aae853ea91ce51efaf012083833ceb563ef2b
Your-Full-Name <praveenpkg8@gmail.com>
2019-11-05 06:07:48 +0530

    implemented constants in go lang

---
+++ constants_04/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	const series = "game of throne"
+	const season = 8
+
+	const jon, nightsWatch = "snow", true
+
+	const stark string = "winter is comming"
+	const dragon int64 = 3
+
+	fmt.Println(series, season, jon, nightsWatch, stark, dragon)
+
+}
